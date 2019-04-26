d82d1bb6b169dfe754d251a47177fb83f63e322f
Tiffany <Tafby88@gmail.com>
2019-04-25 21:51:58 -0700

    back to working product

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
+  post 'find_user' => 'users#find'
+
   resources :users
-  post '/login', to: 'auth#create'
-  get '/profile', to: 'users#profile'
+  # post '/login', to: 'auth#create'
+  # get '/profile', to: 'users#profile'
 
 end
